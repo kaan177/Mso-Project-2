767a8924934ded38385522d7250531854eda18a3
kaan177 <66557991+kaan177@users.noreply.github.com>
2024-10-29 15:44:33 +0100

    fixed dependecies

--- Scratch-Mini/Scratch-Mini-Tester/UnitTest1.cs
+++ Scratch-Mini/Scratch-Mini-Tester/UnitTest1.cs
@@ -1,11 +1,13 @@
+using ScratchMini;
 namespace Scratch_Mini_Tester
+
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
-
+            
         }
     }
 }
