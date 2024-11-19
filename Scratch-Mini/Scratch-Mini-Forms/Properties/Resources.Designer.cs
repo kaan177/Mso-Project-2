2d755b8877969c93bea675362b90272eda47d9d6
kaan177 <66557991+kaan177@users.noreply.github.com>
2024-11-19 12:51:13 +0100

    commit

--- Scratch-Mini/Scratch-Mini-Forms/Properties/Resources.Designer.cs
+++ Scratch-Mini/Scratch-Mini-Forms/Properties/Resources.Designer.cs
@@ -63,9 +63,9 @@ namespace Scratch_Mini_Forms.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Designer {
+        internal static System.Drawing.Bitmap Designer1 {
             get {
-                object obj = ResourceManager.GetObject("Designer", resourceCulture);
+                object obj = ResourceManager.GetObject("Designer1", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
